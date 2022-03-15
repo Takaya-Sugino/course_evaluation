622c5288c58d22e19b31f208c45c41faf3fc8836
anonymous anonymous <takaya.sugino@kellogg.northwestern.edu>
2022-03-15 19:15:10 +0000

    Generate graphiti resources for professors

---
+++ app/resources/professor_resource.rb
@@ -0,0 +1,12 @@
+class ProfessorResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :name, :string
+  attribute :profile_url, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
