b79b3422d9a12aee528197e53de5d173738f400c
anonymous anonymous <takaya.sugino@kellogg.northwestern.edu>
2022-03-15 19:15:08 +0000

    Generate graphiti resources for users

---
+++ app/resources/user_resource.rb
@@ -0,0 +1,16 @@
+class UserResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :name, :string
+  attribute :program, :string
+  attribute :graduation_year, :string
+  attribute :interest, :string
+  attribute :email, :string
+  attribute :password, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
