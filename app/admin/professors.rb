e1bac24ce074e54ef3eef32aedba910b1c66dbba
anonymous anonymous <takaya.sugino@kellogg.northwestern.edu>
2022-03-15 19:14:44 +0000

    Whitelist columns in Professor for ActiveAdmin

--- app/admin/professors.rb
+++ app/admin/professors.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Professor do
 
+ permit_params :name, :profile_url
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
